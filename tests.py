1890b36e0d9a913a43bbf3961d949d4b00f98a00
rhianneadreanagranados-svg <rhianneadreana.granados@ust.edu.ph>
2026-09-04 13:26:35 +0800

    feat: create StressStrainTest class

--- tests.py
+++ tests.py
@@ -36,3 +36,53 @@ class Material:
         return (
             self.name == other.name
             and self.properties == other.properties)
+
+class StressStrainTest:
+    def __init__(
+        self,
+        material: Material,
+        force: float,
+        area: float,
+        original_length: float,
+        change_in_length: float,):
+        self.material = material
+        self._force = force
+        self._area = area
+        self._original_length = original_length
+        self._change_in_length = change_in_length
+
+        if force <= 0:
+            raise ValueError("Force must be positive")
+        if area <= 0:
+            raise ValueError("Area must be positive")
+        if original_length <= 0:
+            raise ValueError("Original length must be positive")
+
+    @property
+    def stress(self) -> float:
+        return self._force / self._area
+
+    @property
+    def strain(self) -> float:
+        return self._change_in_length / self._original_length
+
+    @property
+    def youngs_modulus(self) -> Optional[float]:
+        if self.strain == 0:
+            return None
+        return (self.stress / self.strain) / 1000
+
+    def will_fail(self) -> bool:
+        return not self.material.can_withstand_stress(
+            self.stress)
+
+    def __str__(self) -> str:
+        if self.youngs_modulus is None:
+            modulus = "Undefined"
+        else:
+            modulus = f"{self.youngs_modulus:.2f} GPa"
+        return (
+            f"Test on {self.material.name}: "
+            f"Stress={self.stress:.2f} MPa, "
+            f"Strain={self.strain:.6f}, "
+            f"Young's Modulus={modulus}")
